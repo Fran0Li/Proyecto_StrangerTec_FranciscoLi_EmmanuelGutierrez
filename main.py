9aeac49a27d0d51c72092546424848d35f80b4d5
Francisco Li <liruizfrancisco@gmail.com>
2026-04-30 23:14:35 -0600

    Ajuste en los patrones debido al cambio en el hardware, ademas se sigue con el ajuste general

--- main.py
+++ main.py
@@ -42,35 +42,85 @@ MORSE_DICC = {
     '-....-': '-'}
 #Patron en la maqueta
 #La letrac(bits del registo, led14, led15, led 16) es decir, las filas
-PATRONES_LEDMAQUE = { 'A': ('1000000000000000', 1, 0, 0),
-    'B': ('0100000000000000', 1, 0, 0),
-    'C': ('0010000000000000', 1, 0, 0),
-    'D': ('0001000000000000', 1, 0, 0),
-    'E': ('0000100000000000', 1, 0, 0),
-    'F': ('0000010000000000', 1, 0, 0),
-    'G': ('0000001000000000', 1, 0, 0),
-    'H': ('0000000100000000', 1, 0, 0),
-    'I': ('0000000010000000', 1, 0, 0),
-    'J': ('0000000001000000', 1, 0, 0),
-    'K': ('0000000000100000', 1, 0, 0),
-    'L': ('0000000000010000', 1, 0, 0),
-    'M': ('0000000000001000', 1, 0, 0),
-    'N': ('0000000000000100', 0, 1, 0),
-    'O': ('1111111111111111', 1, 1, 1), # Prende todo
-    'P': ('0000000000000010', 0, 1, 0),
-    'Q': ('0000000000000001', 0, 1, 0),
-    'R': ('1100000000000000', 0, 1, 0),
-    'S': ('0011000000000000', 0, 1, 0),
-    'T': ('0000110000000000', 0, 1, 0),
-    'U': ('0000001100000000', 0, 1, 0),
-    'V': ('0000000011000000', 0, 0, 1),
-    'W': ('0000000000110000', 0, 0, 1),
-    'X': ('0000000000001100', 0, 0, 1),
-    'Y': ('0000000000000011', 0, 0, 1),
-    'Z': ('1010101010101010', 0, 0, 1),
-    '+': ('0000111110000000', 0, 1, 0),
-    '-': ('1111111111111111', 0, 0, 0) }
+#Mapa de bits debido a los cambios en la parte física
+#Cada "C" se refiere a un led que representa una columna
+#Registro1:[C1, C2, C3, C4, 0, C6, C7, C8] el 0 en los 8 bits es el pin 10 (Q4) muerto
+#Registro2:[ 0, C10, C11, C12,  0, C13, C9, C5] -> El 0 inicial es el Pin 3 muerto, el 2do 0 es el Pin 10.
 
+PATRONES_LEDMAQUE = { 
+    # Formato: 'LETRA': (Bits_Reg1, Bits_Reg2, Fila1, Fila2, Fila3)
+    'A': ('10000000', '00000001', 1, 0, 0), # Prende C1 y C5
+    'B': ('01000000', '00000000', 1, 0, 0), # C2
+    'C': ('00100000', '00000000', 1, 0, 0), # C3
+    'D': ('00010000', '00000000', 1, 0, 0), # C4
+    'E': ('00000000', '10000000', 1, 0, 0), # C5 (Reg2, Pin 13)
+    'F': ('00000100', '00000000', 1, 0, 0), # C6
+    'G': ('00000010', '00000000', 1, 0, 0), # C7
+    'H': ('00000001', '00000000', 1, 0, 0), # C8
+    'I': ('00000000', '00000100', 1, 0, 0), # C9 (Reg2, Pin 12)
+    'J': ('00000000', '01000000', 1, 0, 0), # C10
+    'K': ('00000000', '00100000', 1, 0, 0), # C11
+    'L': ('00000000', '00010000', 1, 0, 0), # C12
+    'M': ('00000000', '00000100', 1, 0, 0), # C13
+    'N': ('10000000', '00000000', 0, 1, 0), # Ejemplo fila 2
+    'O': ('11110111', '01110111', 1, 1, 1), # TODO lo que funciona prendido
+    'P': ('01000000', '00000000', 0, 1, 0),
+    'Q': ('00100000', '00000000', 0, 1, 0),
+    'R': ('00010000', '00000000', 0, 1, 0),
+    'S': ('00000000', '10000000', 0, 1, 0),
+    'T': ('00000100', '00000000', 0, 1, 0),
+    'U': ('00000010', '00000000', 0, 1, 0),
+    'V': ('00000001', '00000000', 0, 0, 1), # Fila 3
+    'W': ('00000000', '00000100', 0, 0, 1),
+    'X': ('00000000', '01000000', 0, 0, 1),
+    'Y': ('00000000', '00100000', 0, 0, 1),
+    'Z': ('00000000', '00010000', 0, 0, 1),
+    '+': ('11110000', '10000000', 0, 1, 0),
+    '-': ('11110111', '01110111', 0, 0, 0),
+    'LIMPIAR': ('00000000', '00000000', 0, 0, 0)
+}
+
+# FUNCIONES DE APOYO (LEDs y SONIDO)
+def actualizar_maqueta(letra):
+    """Esta función 'salta' aquí cuando hay una letra lista"""
+    if letra in PATRONES_LEDMAQUE:
+        reg1_bits, reg2_bits, l1, l2, l3 = PATRONES_LEDMAQUE[letra]
+        #Registro 1 (leds de las columnas 1-8 excluyendo la 5)
+        #Se recorre la cadena de texto de bits de izquierda a derecha
+        for bit in reg1_bits: 
+            AB1.value(int(bit))     # Pone el valor del bit (0 o 1) en el pin de datos
+            CLK1.value(1)           # Genera un pulso de reloj (flanco de subida)
+            CLK1.value(0)           # Baja el reloj para preparar el siguiente bit
+        #Registro 2 (Columnas 5 y 9-13, la nueve al pin 12 y la 5 al pin 13)
+        #Se repite la lógica del primero
+            for bit in reg2_bits:
+            AB2.value(int(bit))     
+            CLK2.value(1)           
+            CLK2.value(0)
+            
+        #Control de las Filas
+        #Leds de filas encienden con un 0 (tierra)
+        #Si el dicc dice 1 (encender), se manda un 0 al pin
+        led14.value(0 if l1 == 1 else 1) # Fila 1 (GP15)
+        led15.value(0 if l2 == 1 else 1) # Fila 2 (GP14)
+        led16.value(0 if l3 == 1 else 1) # Fila 3 (GP13)
+    else:
+        #si la letra no existe: se limpia la maqueta
+        # se envian los 8 ceros a los dos registros
+        for _ in range(8):
+            AB1(0); CLK1(1); CLK1(0)
+            AB2(0); CLK2(1); CLK2(0)
+        # Apaga las filas poniendo los pines en 1 (reposo).
+        led14.value(1); led15.value(1); led16.value(1)
+      
+
+def sonar_buzzer(estado):
+    """Hace sonar el buzzer a 1kHz cuando el estado es True"""
+    if estado:
+        buzzer.freq(1000)
+        buzzer.duty_u16(32768) # 50% de ciclo de trabajo (volumen)
+    else:
+        buzzer.duty_u16(0)
 
 # FUNCIONES RASPYCONNECTION
 
@@ -90,29 +140,7 @@ def connectToWifi():
     except:
         print('Algo salio mal. Intente nuevamente')
 
-# 3. FUNCIONES DE APOYO (LEDs y SONIDO)
-def actualizar_maqueta(letra):
-    """Esta función 'salta' aquí cuando hay una letra lista"""
-    if letra in PATRONES_LEDMAQUE:
-        bits_reg, l14, l15, l16 = PATRONES_LEDMAQUE[letra]
-        #Enviar los 16 bits
-        for bit in reversed(bits_reg):
-            AB.value(int(bit))
-            CLK.value(1)
-            CLK.value(0)
-        
-        # Prender/Apagar los LEDs de la Raspy (0=prende, 1=apaga)
-        led14.value(0 if l14 == 1 else 1)
-        led15.value(0 if l15 == 1 else 1)
-        led16.value(0 if l16 == 1 else 1)
 
-def sonar_buzzer(estado):
-    """Hace sonar el buzzer a 1kHz cuando el estado es True"""
-    if estado:
-        buzzer.freq(1000)
-        buzzer.duty_u16(32768) # 50% de ciclo de trabajo (volumen)
-    else:
-        buzzer.duty_u16(0)
 
 # 4. LÓGICA PRINCIPAL (BOTÓN + CLIENTE)
 
